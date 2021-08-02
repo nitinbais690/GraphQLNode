abc12188c13fab8cbacad1c8de24cad9e725999a
mahadevp <mahadev.p@firstlight.ai>
2021-08-02 09:56:42 +0530

    nodemon and company association

--- schema.js
+++ schema.js
@@ -1,38 +1,51 @@
+const graphQL = require("graphql");
+const axios = require("axios");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphQL;
 
-const graphQL = require('graphql');
-const axios =require('axios')
-const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema
-}=graphQL;
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
- const UserType= new GraphQLObjectType({
-    name: 'User',
-    fields: {
-        id: {type: GraphQLString },
-        firtsName: {type: GraphQLString },
-        lastName: {type: GraphQLString},
-        age: {type: GraphQLInt},
-    }
-})
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firtsName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, agrs) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
 
- const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryQL',
-    fields: {
-        user: {
-            type: UserType,
-            args: {
-                id: { type: GraphQLString}
-            },
-            resolve(parentValue, args){
-                return axios.get(`http://localhost:3000/users/${args.id}`).then(res=> res.data)
-            }
-        }
-    }
-})
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryQL",
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+});
